c622706387b41c438ca81fe948ccc7c2d907ad2a
Jonathan Dyallo <emeraldusk@gmail.com>
2023-03-26 20:45:55 -0300

    ⭐ - Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Debian-package-base
+
+![image](http://wallpapercave.com/wp/xk9cJfn.jpg)
+
+This is a base for creating Debian packages. It is based on [dbn-tools](https://github.com/jd-apprentice/dbn-tools).
---
+++ utils/build_package.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# App name for multiples uses
+app_name="debian-package-base"
+
+# Starting message
+echo "Building latest version of ${app_name}"
+
+# Find version of the package and save it in a variable
+file=$(find . -name 'control')
+string="Version"
+
+while read -r line; do
+    if [[ $line =~ $string ]]; then
+        version=${line:(-3):3}
+    fi
+done <$file
+
+# We locate where the folder is located and move to it
+folder=$(find $HOME -type d -name "${app_name}" -print | tail -n 1)
+cd "$folder"
+
+# We create a folder for the build and copy the entire file one level above
+cd ..
+mkdir -p -- "${version}-${app_name}"
+copy=$(find $HOME -type d -name "${version}-${app_name}")
+cp -r $folder/* "$copy"
+
+# Move to the copy directory
+cd "$copy"
+
+# We remove git since we don't want it in the final build
+# Also here remove anything not useful to the final build example images for the readme, markdown files, etc
+rm -rf .git
+rm *.md
+
+# Move out of the copy directory
+cd ..
+
+# Build latest version of the package
+dpkg --build "${version}-${app_name}"
